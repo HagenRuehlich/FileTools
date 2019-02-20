a1114386c8cd486ca194061e8db8992ff5bfc749
HagenRuehlich <hagen.ruehlich@gmx.de>
2019-02-20 19:34:51 +0100

    Check prepared
    Here a queck is required if the files are possibly already renamed. In this case the user should decided if he wants to rename again or not... This requirements need ti be implemented finally.

--- File_Photo_Renamer.py
+++ File_Photo_Renamer.py
@@ -4,18 +4,27 @@
 from FileUtilities import *
 from FileTraverser import *
 from tkinter import filedialog
+from tkinter import messagebox
 from tkinter import *
 
-c_BASE_PHOTO_FOLDER = "K:\\Datenbereich\\Fotos"
+c_BASE_PHOTO_FOLDER = "K:\\Datenbereich\\Fotos\\Fotos 2019"
 
 def renamePhotos ():
     sFolderToRename = selectFolder ()
     if sFolderToRename != "":
+##        bFileAlreadyRenamed =  areFilesAleadyRenamed (sFolderToRename)
+##        if bFileAlreadyRenamed :
+##            ask the user if to go in case the files in selected folder seem to be renamed already
+            
         oRename =  CFileTraverserPhotoRenamer (sFolderToRename)
         oRename.travers ()
         
     
-    
+def areFilesAleadyRenamed (psFoldertoCheck):
+    bFilesAlreadyRenamed = False
+    verifyPath (psFoldertoCheck)
+    #Go on here...
+    return bFilesAlreadyRenamed
     
 
 def selectFolder ():
@@ -25,6 +34,8 @@ def selectFolder ():
     sReturnFolder = filedialog.askdirectory(initialdir = c_BASE_PHOTO_FOLDER, title='Ordner mit Fotos zum umbenennen auswählen')
     if len(sReturnFolder) > 0:
         print ("You chose %s" % sReturnFolder)
+    else:
+        print ("Kein Ornder gewählt. Abbruch")
     return sReturnFolder
 
 
